d02d5729ddccbae71085d8f4a403657024184dde
Myrrolinz <mildred1195564913@gmail.com>
2023-02-05 16:52:11 +0800

    Initial
    about view

---
+++ frontend/views/layouts/blog.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * Team:ddl驱动队,NKU
+ * coding by sunyiqi 2012810,20230201
+ */
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use frontend\assets\AppAsset;
+use yii\helpers\Html;
+use yii\bootstrap4\Nav;
+use yii\bootstrap4\NavBar;
+use yii\widgets\Breadcrumbs;
+use common\widgets\Alert;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php $this->registerCsrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
+    <?php $this->head() ?>
+
+</head>
+
+<body>
+    <?php $this->beginBody() ?>
+
+    <div class="wrap h-100 d-flex flex-column">
+        <?php echo $this->render('header') ?>
+
+
+        
+
+        <?= $content ?>
+
+
+    </div>
+
+
+
+    <?php $this->endBody() ?>
+
+</body>
+
+</html>
+<?php $this->endPage() ?>
---
+++ frontend/views/site/about.php
@@ -0,0 +1,148 @@
+<?php
+
+/**
+ * Team:ddl驱动队,NKU
+ * coding by sunyiqi 2012810,20230201
+ */
+
+
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+
+$this->title = 'About';
+$this->params['breadcrumbs'][] = $this->title;
+
+
+?>
+
+<?php echo Html::cssFile('@web/css/default.css'); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html>
+
+<head>
+	<meta charset='UTF-8'>
+	<style>
+		* {
+			margin: 0;
+			padding: 0;
+		}
+
+		.box {
+			position: relative;
+			width: 1300px;
+			height: 640px;
+			/*框在页面里的位置*/
+			border: 10px solid rgb(250, 252, 253);
+			margin: 0px auto;
+			background-repeat: no-repeat;
+			overflow: hidden;
+			background-size: 50%;
+			z-index: 0;
+		}
+
+		/*.box img{
+			width: 900px;
+			height: 340px;
+		}*/
+		#pic {
+			width: 1300px;
+			height: 640px;
+			background-repeat: no-repeat;
+			background-size: 100% 100%;
+		}
+
+		.btn1 {
+			position: relative;
+			width: 100px;
+			height: 35px;
+			left: 35.7%;
+			top: 45%;
+			-webkit-transition-duration: 0.4s;
+			transition-duration: 0.4s;
+			text-align: center;
+			background-color: white;
+			color: black;
+			border: 2px solid rgb(135, 135, 135);
+			border-radius: 5px;
+			z-index: 2;
+		}
+
+		.btn1:hover {
+			background-color: rgb(135, 135, 135);
+			color: white;
+		}
+	</style>
+</head>
+
+<head>
+	<meta charset='UTF-8'>
+	<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+	<meta name="description" content="description" />
+	<meta name="keywords" content="keywords" />
+	<meta name="author" content="Arcsin, www.arcsin.se" />
+	<link rel="stylesheet" type="text/css" href="default.css" />
+	<title>COVID</title>
+</head>
+
+<body>
+	<div class="box">
+		<img id="pic" src="../../../frontend/web/img/0.jpg" />
+	</div>
+	<div>
+		<input type="button" class="btn1" value="上一张" />
+		<input type="button" class="btn1" value="下一张" />
+	</div>
+	<script>
+		var pic = document.getElementById("pic");
+		var preBtn = document.getElementsByClassName("btn1")[0];
+		var nextBtn = document.getElementsByClassName("btn1")[1];
+		//			
+		preBtn.onclick = function() {
+			n--;
+			if (n == 0) {
+				n = 6;
+			}
+			pic.src = "../../../frontend/web/img/" + n + ".jpg"
+		}
+		nextBtn.onclick = function() {
+			picLunH();
+		}
+		var n = 1;
+
+		function picLunH() {
+			n++;
+			if (n == 7) {
+				n = 1;
+			}
+			pic.src = "../../../frontend/web/img/" + n + ".jpg"
+
+		}
+		setInterval(picLunH, 3000);
+	</script>
+
+	<div class="main">
+		<div class="header_left">
+			<div class="menu">
+				<div class="gfx_nav"><span></span></div>
+				<a href="../../../frontend/web/site/about"><span>01</span> 首页</a>
+				<a href="../../../frontend/web/site/about1"><span>02</span> 起因</a>
+				<a href="../../../frontend/web/site/about2"><span>03</span> 现状</a>
+			</div>
+			<a href="../../../frontend/web/site/about" id="big">Russia and Ukraine</a>
+		</div>
+
+		<div class="content">
+			<h1><span>俄乌战争</span></h1>
+			<p> </p>
+			<p>这是战争
+			<p> </p>
+		</div>
+	</div>
+	<script type="text/javascript" src="../../../frontend/web/js/canvas-nest.min.js"></script>　　
+	<script type="text/javascript" src="../../../frontend/web/js/emojiCursor.js"></script>　
+</body>
+
+</html>
